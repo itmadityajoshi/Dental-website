74d0d4d933379cddfc50a8bdbd7c69fb504b7d30
unknown <adityajoshi316@gmail.com>
2026-08-12 22:40:20 +0200

    clinic models created

--- clinic/models.py
+++ clinic/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Dentist(models.Model):
+    name = models.CharField(max_length=50)
+    specialization = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="dentist_photo/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    duration_minutes = models.PositiveIntegerField(default=30)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
