d55de45fab496f0a4cea9bfd52edc63bfdfcb3cf
unknown <adityajoshi316@gmail.com>
2026-08-17 23:30:53 +0200

    appointment date booking validation added

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.utils import timezone
 from .models import Appointment
 
 
@@ -7,3 +8,9 @@ class AppointmentSerializer(serializers.ModelSerializer):
         model = Appointment
         fields = '__all__'
         read_only_fields = ["patient","status", "created_at"]
+
+
+    def validate_date(self, value): # it will prevent from booking an appointment from the past date.
+        if value < timezone.now().date():
+            raise serializers.ValidationError("Appointment date cannot be in the past. ")
+        return value
