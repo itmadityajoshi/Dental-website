edd9a30308d1946063379101e9707fa09fcfb374
unknown <adityajoshi316@gmail.com>
2026-09-14 23:04:18 +0200

    changed in cors url

--- backend/Dental_clinic/settings.py
+++ backend/Dental_clinic/settings.py
@@ -55,10 +55,7 @@ CORS_ALLOWED_ORIGINS = [
     origin.strip()
     for origin in os.getenv(
         "CORS_ALLOWED_ORIGINS",
-        "http://localhost:3000," 
-        "http://localhost:5173," 
-        "https://dental-website-eta-eosin.vercel.app",
-        "https://dental-website-git-main-itmadityajoshis-projects.vercel.app"
+        ""
     ).split(",")
     if origin.strip()
 ]
