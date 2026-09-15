5f230eb3bbc1febb7e105e7b26e1f9ecc4c9e0a0
unknown <adityajoshi316@gmail.com>
2026-09-15 13:37:35 +0200

    Fix production CORS Settings

--- backend/Dental_clinic/settings.py
+++ backend/Dental_clinic/settings.py
@@ -56,8 +56,6 @@ CORS_ALLOWED_ORIGINS = [
         "http://localhost:5173",
         "https://dental-website-eta-eosin.vercel.app",
         "https://dental-website-git-main-itmadityajoshis-projects.vercel.app"
-
-
 ]
 
 CSRF_TRUSTED_ORIGINS =[
